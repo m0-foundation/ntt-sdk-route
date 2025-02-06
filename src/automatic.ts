94d2571c774ea83708ff0ea23fa30dbe9c82c5bc
0xIryna <iryna.hubernska@m0.xyz>
2025-02-06 09:11:10 -0800

    feat: custom route draft

--- src/automatic.ts
+++ src/automatic.ts
@@ -1,5 +1,9 @@
-import { amount, ChainAddress, Network, routes, Signer, signSendWait, TransferState, Wormhole } from "@wormhole-foundation/sdk-connect";
+import { AccountAddress, amount, ChainAddress, chainToPlatform, Network, routes, Signer, signSendWait, toChainId, TransferState, universalAddress, Wormhole } from "@wormhole-foundation/sdk-connect";
+import { addChainId, addFrom, EvmAddress, EvmChains, EvmPlatform, EvmUnsignedTransaction } from "@wormhole-foundation/sdk-evm";
 import { NttAutomaticRoute, NttRoute } from "@wormhole-foundation/sdk-route-ntt";
+import { TransactionRequest } from "ethers";
+import type { EvmNtt } from "@wormhole-foundation/sdk-evm-ntt";
+import { Ntt } from "@wormhole-foundation/sdk-definitions-ntt";
 
 type Op = NttRoute.Options;
 type Vp = NttRoute.ValidatedParams;
@@ -7,6 +11,75 @@ type Q = routes.Quote<Op, Vp>;
 type R = NttRoute.AutomaticTransferReceipt;
 
 export class M0AutomaticRoute<N extends Network> extends NttAutomaticRoute<N> {
+
+    createUnsignedTx<N extends Network, C extends EvmChains>(
+        ntt: EvmNtt<N, C>,
+        txReq: TransactionRequest,
+        description: string,
+        parallelizable: boolean = false
+    ): EvmUnsignedTransaction<N, C> {
+        return new EvmUnsignedTransaction(
+            addChainId(txReq, ntt.chainId),
+            ntt.network,
+            ntt.chain,
+            description,
+            parallelizable
+        );
+    }
+
+    /**
+        * Modified from EvmNtt `transfer` function to call `transferMLikeToken` instead
+        * https://github.com/wormhole-foundation/native-token-transfers/blob/main/evm/ts/src/ntt.ts#L461
+    */
+    async *transferMLike<N extends Network, C extends EvmChains>(
+        ntt: EvmNtt<N, C>,
+        sender: AccountAddress<C>,
+        amount: bigint,
+        destination: ChainAddress,
+        sourceToken: string,
+        destinationToken: string,
+        options: Ntt.TransferOptions
+    ): AsyncGenerator<EvmUnsignedTransaction<N, C>> {
+        const senderAddress = new EvmAddress(sender).toString();
+        // Note: these flags are indexed by transceiver index
+        const totalPrice = await ntt.quoteDeliveryPrice(
+            destination.chain,
+            options
+        );
+
+        //TODO check for ERC-2612 (permit) support on token?
+        const tokenContract = EvmPlatform.getTokenImplementation(
+            ntt.provider,
+            ntt.tokenAddress
+        );
+
+        const allowance = await tokenContract.allowance(
+            senderAddress,
+            ntt.managerAddress
+        );
+        if (allowance < amount) {
+            const txReq = await tokenContract.approve.populateTransaction(
+                ntt.managerAddress,
+                amount
+            );
+            yield this.createUnsignedTx(ntt, addFrom(txReq, senderAddress), "Ntt.Approve");
+        }
+        const receiver = universalAddress(destination);
+        const txReq = await ntt.manager
+            .getFunction("function transferMLikeToken(uint256 amount, address sourceToken, uint16 destinationChainId, bytes32 destinationToken, bytes32 recipient, bytes32 refundAddress) external payable returns (uint64 sequence)")
+            .populateTransaction(
+                amount,
+                sourceToken,
+                toChainId(destination.chain),
+                destinationToken,
+                receiver,
+                receiver,
+                { value: totalPrice }
+            );
+
+        yield ntt.createUnsignedTx(addFrom(txReq, senderAddress), "Ntt.transfer");
+    }
+
     async initiate(
         request: routes.RouteTransferRequest<N>,
         signer: Signer,
@@ -14,19 +87,30 @@ export class M0AutomaticRoute<N extends Network> extends NttAutomaticRoute<N> {
         to: ChainAddress
     ): Promise<R> {
         const { params } = quote;
-        const { fromChain } = request;
+        const { fromChain, toChain } = request;
         const sender = Wormhole.parseAddress(signer.chain(), signer.address());
 
-        const ntt = await fromChain.getProtocol("Ntt", {
-            ntt: params.normalizedParams.sourceContracts,
-        });
+        // TODO: check if platform is EVM
+        const platform = chainToPlatform(fromChain.chain);
 
-        const initXfer = ntt.transfer(
-            sender,
-            amount.units(params.normalizedParams.amount),
-            to,
-            params.normalizedParams.options
-        );
+        const ntt = (await fromChain.getProtocol("Ntt", {
+            ntt: params.normalizedParams.sourceContracts,
+        })) as EvmNtt<N, EvmChains>;
+
+        // TOD0: check if source or destination token is wrapped token
+        const sourceTokenAddress = params.normalizedParams.sourceContracts.token;
+        const destinationTokenAddress = params.normalizedParams.sourceContracts.token;
+
+        const initXfer = this.transferMLike
+            (ntt,
+                sender,
+                amount.units(params.normalizedParams.amount),
+                to,
+                sourceTokenAddress,
+                destinationTokenAddress,
+                params.normalizedParams.options
+            );
+
         const txids = await signSendWait(fromChain, initXfer, signer);
 
         return {
